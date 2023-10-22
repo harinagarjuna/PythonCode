0c575b6e364305a8d2700397f5f29fc0cd55ff4a
harinagarjuna <147254426+harinagarjuna@users.noreply.github.com>
2023-10-22 15:18:08 +0530

    Add files via upload

---
+++ 18OCT2023/Break option.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the number to print the series"))
+num2 = int(input("Enter the breaking number"))
+
+for i in range(num):
+    if i == num2 :
+        print(i)
+        break
+    print(i)
+
---
+++ 18OCT2023/Fibonacci.py
@@ -0,0 +1,13 @@
+value = int(input("Enter a number"))
+n1 = 0
+n2 = 1
+lv = 0
+while lv<=value:
+    print(n1)
+    sum = n1+n2
+    n1 = n2
+    n2 = sum
+    lv = lv+1
+
+
+
